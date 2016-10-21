a985e0a0ebba0f290819ca213871de75742e0259
Lobato <iagogcostah@Gmail.com>
2016-10-21 15:07:10 -0200

    Create entrada.js

---
+++ entrada.js
@@ -0,0 +1,26 @@
+API.on(API.USER_JOIN, function(user) {
+/*if (user.id == "4080795") {
+API.sendChat("/me Shaaadoso <3 ");
+}*/
+
+if (user.id == "5401598") {
+API.sendChat("/me   @" + user.username + " Musa do Plug e Rainha da Move It acaba de entrar na sala.:crown:");
+}
+
+if (user.id == "1380997") {
+API.sendChat("/me Preparem-se, o martelo do BAN  @" + user.username + " acabou de entrar!.");
+}
+
+if (user.id == "4371581") {
+API.sendChat("/me O melhor segurança  @" + user.username + " entrou na sala!.");
+}     
+
+if (user.id == "6511093") {
+API.sendChat("/me A mulher do xado @" + user.username + " acaba de entrar na sala.");
+}
+
+if (user.id == "19711306") {
+API.sendChat("/me O viadinho mais rodado do plug @" + user.username + " acaba de entrar na sala.");
+}      
+
+});
